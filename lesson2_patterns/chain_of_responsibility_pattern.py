cb836ef6ce30599bc11c3718f5ea8d525ee168f6
Маркелов Родион <r.ziuzev@bars.group>
2024-09-05 11:27:10 +0300

    Remove multiple exit points from method to one; add result field to expression

--- lesson2_patterns/chain_of_responsibility_pattern.py
+++ lesson2_patterns/chain_of_responsibility_pattern.py
@@ -6,6 +6,7 @@ class ArithmeticOperation:
     """Handler interface"""
     def __init__(self):
         self._next_handler = None
+        self.result = None
 
     def set_next(self, handler):
         self._next_handler = handler
@@ -13,7 +14,9 @@ class ArithmeticOperation:
 
     @abstractmethod
     def calculate(self, expression):
-        if self._next_handler:
+        if 'result' in expression:
+            self.result = expression['result']
+        elif self._next_handler:
             return self._next_handler.calculate(expression)
 
 
@@ -21,21 +24,21 @@ class PlusOperation(ArithmeticOperation):
     """Concrete handlers"""
     def calculate(self, expression):
         if expression['operation'] == '+':
-            return expression['ints'][0] + expression['ints'][1]
+            expression['result'] = (expression['ints'][0] + expression['ints'][1])
         return super().calculate(expression)
 
 
 class MinusOperation(ArithmeticOperation):
     def calculate(self, expression):
         if expression['operation'] == '-':
-            return expression['ints'][0] - expression['ints'][1]
+            expression['result'] = (expression['ints'][0] - expression['ints'][1])
         return super().calculate(expression)
 
 
 class MultiplyOperation(ArithmeticOperation):
     def calculate(self, expression):
         if expression['operation'] == '*':
-            return expression['ints'][0] * expression['ints'][1]
+            expression['result'] = (expression['ints'][0] * expression['ints'][1])
         return super().calculate(expression)
 
 
@@ -43,7 +46,7 @@ class DivideOperation(ArithmeticOperation):
     def calculate(self, expression):
         if expression['operation'] == '/':
             if expression['ints'][1] != 0:
-                return expression['ints'][0] / expression['ints'][1]
+                expression['result'] = (expression['ints'][0] / expression['ints'][1])
             else:
                 raise ZeroDivisionError('Ошибка: деление на ноль невозможно')
         return super().calculate(expression)
@@ -73,7 +76,8 @@ def main():
     for expression in expressions:
         transformed_exp = transform_validate(expression)
         if transformed_exp:
-            print(f"{expression} = {add_handler.calculate(transformed_exp)}")
+            add_handler.calculate(transformed_exp)
+            print(f"{expression} = {transformed_exp['result']}")
         else:
             raise ValueError('Ошибка: выражение некорректно')
 
